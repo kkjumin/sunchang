ceb85c4fe73ccf65ec395b5bcecf477064b36bf0
kkjumin <kkjumin@gmail.com>
2020-11-12 17:45:59 +0900

    babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    [
+      '@vue/app',
+      {
+        useBuiltIns: 'entry'
+      }
+    ],
+    ['@babel/preset-env']
+  ],
+  plugins: [
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining'
   ]
 };
