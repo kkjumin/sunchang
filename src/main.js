74ef106bfdb939ed9d92c7076aaf05f4d3f149bc
kkjumin <kkjumin@gmail.com>
2020-11-12 16:49:03 +0900

    axios, lodash

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
+import '@babel/polyfill';
 import Vue from 'vue';
+import VueLodash from 'vue-lodash';
+import lodash from 'lodash';
+import axios from 'axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+// lodash
+Vue.use(VueLodash, { name: 'custom', lodash });
+// axios
+Vue.prototype.$http = axios;
+// eventBus
+Vue.prototype.$bus = new Vue();
 
 new Vue({
   router,
