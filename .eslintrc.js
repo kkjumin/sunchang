586bca2a93db54e8d818d3f95dcd938f94f4ea01
kkjumin <kkjumin@gmail.com>
2020-11-13 17:59:34 +0900

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    'linebreak-style': ['error', 'windows'],
-    'comma-dangle': ['error', 'never']
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/airbnb',
+        'plugin:prettier/recommended'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-plusplus': ['error', { allowForLoopAfterthoughts: true }]
+    }
 };
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    arrowParens: 'avoid',
+    trailingComma: 'none',
+    singleQuote: true,
+    tabWidth: 4,
+    semi: true,
+    requireConfig: true,
+    printWidth: 80,
+    packageManager: 'yarn',
+    endOfLine: 'crlf'
+};
