b176a0c75f9e47c72e5f7a63ce622e4f22305aeb
kkjumin <kkjumin@gmail.com>
2020-11-17 13:39:51 +0900

    VueQuillEditor

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import '@babel/polyfill';
 import Vue from 'vue';
 import VueLodash from 'vue-lodash';
 import lodash from 'lodash';
 import axios from 'axios';
+import VueQuillEditor from 'vue-quill-editor';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 import '@/assets/css/reset.css';
+import 'quill/dist/quill.core.css';
+import 'quill/dist/quill.snow.css';
+import 'quill/dist/quill.bubble.css';
 
 Vue.config.productionTip = false;
 // lodash
 Vue.use(VueLodash, { name: 'custom', lodash });
+Vue.use(VueQuillEditor /* { default global options } */);
 // axios
 Vue.prototype.$http = axios;
 // eventBus
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,62 @@
 <template>
-    <div class="about">
-        <h1>This is an about page</h1>
+    <div class="about" style="width: 1000px; height: 1000px; margin: 0 auto">
+        <div class="example">
+            <quill-editor
+                style="height: 500px"
+                class="editor"
+                ref="myTextEditor"
+                :value="content"
+                :options="editorOption"
+                @change="onEditorChange"
+            />
+        </div>
     </div>
 </template>
+
+<script>
+// import NanumBarunGothic from '@/assets/fonts/NanumBarunGothic.ttf';
+
+export default {
+    data() {
+        return {
+            content: '',
+            editorOption: {
+                modules: {
+                    toolbar: [
+                        ['bold', 'italic', 'underline'],
+                        // ['blockquote', 'code-block'],
+                        [{ header: 1 }, { header: 2 }],
+                        // [{ list: 'ordered' }, { list: 'bullet' }],
+                        // [{ script: 'sub' }, { script: 'super' }],
+                        [{ indent: '-1' }, { indent: '+1' }],
+                        // [{ direction: 'rtl' }],
+                        [{ size: ['small', false, 'large', 'huge'] }],
+                        // [{ header: [1, 2, 3, 4, 5, 6, false] }],
+                        // [{ font: [] }],
+                        [{ color: [] }, { background: [] }],
+                        [{ align: [] }],
+                        // ['clean'],
+                        ['link', 'image', 'video']
+                    ]
+                },
+                placeholder: '내용을 입력하세요...'
+            }
+        };
+    },
+    methods: {
+        onEditorChange({ quill, html, text }) {
+            console.log('editor change!', quill, html, text);
+            this.content = html;
+        },
+        summit() {
+            console.log(this.content);
+        }
+    },
+    computed: {
+        editor() {
+            return this.$refs.myTextEditor.quill;
+        }
+    },
+    mounted() {}
+};
+</script>
